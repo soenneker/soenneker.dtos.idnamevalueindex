3afa8ac33a2e03eb14f751d1fc41031f48a2d5b1
Jake Soenneker <jake@soeneker.com>
2024-12-22 13:58:56 +0000

    Tests

---
+++ test/Soenneker.Dtos.IdNameValueIndex.Tests/IdNameValueIndexTests.cs
@@ -0,0 +1,18 @@
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Dtos.IdNameValueIndex.Tests;
+
+[Collection("Collection")]
+public class IdNameValueIndexTests : FixturedUnitTest
+{
+    public IdNameValueIndexTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
