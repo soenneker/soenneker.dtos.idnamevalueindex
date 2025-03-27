244152c74eebfe0e5099f3b5902aa012f0cf2591
Jake Soenneker <jake@soenneker.com>
2025-03-26 20:26:56 -0500

    cleanup

--- src/IdNameValueIndex.cs
+++ src/IdNameValueIndex.cs
@@ -1,5 +1,4 @@
 using Newtonsoft.Json;
-using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 
 namespace Soenneker.Dtos.IdNameValueIndex;
@@ -7,10 +6,8 @@ namespace Soenneker.Dtos.IdNameValueIndex;
 /// <summary>
 /// A minimal Record type with an Id (string), Name (string), Value (string?), and Index (string?) and maximum JSON compatibility
 /// </summary>
-[DataContract]
 public record IdNameValueIndex : IdNameValue.IdNameValue
 {
-    [DataMember(Name = "index")]
     [JsonPropertyName("index")]
     [JsonProperty("index")]
     public string? Index { get; set; }
