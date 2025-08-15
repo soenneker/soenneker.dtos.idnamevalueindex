1e1e504a123c15ab81f269349edbb2c18d9346ff
Jake Soenneker <jake@soenneker.com>
2025-08-14 19:51:04 -0500

    PublicOpenApiObject

--- src/IdNameValueIndex.cs
+++ src/IdNameValueIndex.cs
@@ -1,11 +1,13 @@
 using Newtonsoft.Json;
 using System.Text.Json.Serialization;
+using Soenneker.Attributes.PublicOpenApiObject;
 
 namespace Soenneker.Dtos.IdNameValueIndex;
 
 /// <summary>
 /// A minimal Record type with an Id (string), Name (string), Value (string?), and Index (string?) and maximum JSON compatibility
 /// </summary>
+[PublicOpenApiObject]
 public record IdNameValueIndex : IdNameValue.IdNameValue
 {
     [JsonPropertyName("index")]
